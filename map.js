4fa4c870b295a9e52c0cdd7876cf7083e02bc0ec
Dhruv Goel <40907067+dhruv025@users.noreply.github.com>
2021-07-31 01:09:46 +0530

    Create map.js

---
+++ map.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+
+function App(){
+  const names = ['virat','anushka','rohit','ritika'];
+  names.map((name)=>(
+    console.log(name)
+  ))
+  const details = [
+    {
+      name : 'dhruv', phoneno : 8123456789, gmail : 'dhruv@gmail.com'
+    },
+    {
+      name : 'rohit', phoneno : 8165437889, gmail : 'rohit@gmail.com'
+    },
+    {
+      name : 'virat', phoneno : 8123980389, gmail : 'virat@gmail.com'
+    }
+  ]
+  return(
+    <div className="heading">
+      <h2>ReactJs</h2>
+      {
+        details.map((detail)=>
+          <h2>{detail.name} {detail.phoneno} {detail.gmail}</h2>
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
